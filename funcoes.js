149ece8619aa4ba860c40917725e59eeed11096e
Thayller Vilela Cintra <thayllervilelacintra@yahoo.com.br>
2015-10-16 16:27:25 -0300

    validacao

---
+++ funcoes.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){ 
+      /*########VALIDAÇÃO PERSONALIZADA########*/
+      var cont = 0;
+      $('#frmSac2').submit(function(event){
+
+            event.preventDefault();
+
+            $('#frmSac2 .validar ').each(function(index,el) {
+
+                  $(this).css("border","1px solid #ccc");
+                  $(this).css("background-color","white");  
+
+                  if ( $(this).val().length === 0) {
+
+                        if ($("#"+el.id+"").hasClass( "validar" )){
+
+                            $(this).css("background-color","#f2dede");
+                            $(this).css("border","2px solid red");
+
+                            $(this).popover({
+                                title: "Atenção!",
+                                content: "Preencha o campo.", 
+                                placement: "bottom" // (top, right, bottom, left)
+                            });
+
+                            $(this).popover('show');
+                            setTimeout(function() { $("#"+el.id+"").popover('hide'); }, 2000);  
+                            $(this).focus();
+                            cont = -1;
+
+                            return false;
+                        }else{
+                             cont =1;
+                        }
+                  }
+
+            });
+            if(cont == 1)
+               $(this).unbind('submit').submit();  
+      });
+     
+});
